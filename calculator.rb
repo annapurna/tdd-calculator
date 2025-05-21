5dcd061c8fdc3e47a2bdd3023ed9e44511442d51
annapurna <prapoorna.r@gmail.com>
2025-05-21 11:32:17 +0530

    add method included with arguments and testcase

--- calculator.rb
+++ calculator.rb
@@ -1,2 +1,6 @@
 class Calculator
+
+  def add(numbers)
+  end 
+
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,12 +1,18 @@
 require './calculator.rb'
 
 
-
-
 RSpec.describe Calculator do
 
  it 'class exists' do
    calc = Calculator.new
    expect(calc).to match(a_kind_of(Calculator)) 
- end  
+ end 
+
+ it 'check for method #add' do
+   calc = Calculator.new
+   numbers = '12,3'
+   expect(calc).to receive(:add).with(numbers) 
+   calc.add(numbers)
+ end
+ 
 end
